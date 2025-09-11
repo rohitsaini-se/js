1e53c0e259801fe2713ab395f63e781a6696cf9f
Rohit Saini <emailrohitsaini@gmail.com>
2025-09-11 05:50:23 +0530

    Add new file for demonstrating operations and type coercion in JavaScript

---
+++ source-02/04-operations.js
@@ -0,0 +1,20 @@
+let value = 17;
+let negValue = -value;
+console.log(negValue);
+
+let str1 = "hi";
+let str2 = " Rohit!";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let counter = 1;
+console.log(counter++);
+console.log(++counter);
