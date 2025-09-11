f61ded3b4a792687b298a06d91129578ffac4904
Rohit Saini <emailrohitsaini@gmail.com>
2025-09-11 05:55:06 +0530

    Add new file to demonstrate comparison operations and type coercion in JavaScript

---
+++ source-02/05-comparison.js
@@ -0,0 +1,11 @@
+console.log("2" > 1);
+console.log("02" > 1);
+
+// While comparing, the datatypes of the compared entities should be same to produce predictable results.
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log("2" == 2);
+console.log("2" === 2);
